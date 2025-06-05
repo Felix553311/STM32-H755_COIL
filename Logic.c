bdf07fd1e0bb3407f2b9ad00dc517841b9acc223
Felix553311 <felixbahrke9@gmail.com>
2025-06-05 18:43:09 +0200

    Update Logic.c

--- Logic.c
+++ Logic.c
@@ -93,17 +93,17 @@ uint8_t bird(_Bool);  // Prototyp-decl
 
 
 
-void hit_tstf(Tubesaver *tubes, uint8_t flpy_h){
-	static uint8_t lifes = 3;
-	static unsigned int score;
-	Rect(0,LCD_width,0,LCD_hight,BLACK);
+void hit_tstf(Tubesaver *tubes, uint8_t flpy_h){							// just open the array of structs not copy
+	static uint8_t lifes = 3;									// static makes the variable remember it's value
+	static unsigned int score;									// call the scoreboard variables by pointer in the future
+	Rect(0,LCD_width,0,LCD_hight,BLACK);								// delete whole screen 
 	for(uint8_t i = 0; i < 4; i++){                 					  	//refresh every tubepos in the tubemap
 		if((tubes[i].x > 0) && (tubes[i].x <= LCD_width)){
 			tubes[i].x--;
 
 
 
-			//Line(tubes[i].x + 1,0,tubes[i].gapl, BLACK);			// draw first upper tube      ! the screen is upside down !
+			//Line(tubes[i].x + 1,0,tubes[i].gapl, BLACK);			// add for thickness first upper tube      
 			//Line(tubes[i].x + 1,tubes[i].gaph,239, BLACK);			// draw first lower tube
 
 
@@ -111,32 +111,25 @@ void hit_tstf(Tubesaver *tubes, uint8_t flpy_h){
 			Line(tubes[i].x,tubes[i].gaph,239, GREEN);			// draw first lower tube
 
 
-
-
-
-
 		}
 		if((tubes[i].x == xposofst + 3) ){									// if 1st px of flpy is equal to 1st line of tube make the test
 			if((flpy_h >= tubes[i].gaph)  || (flpy_h <= tubes[i].gapl)){	// test if flpy is outside the gap hight     LCD upside down all values fliped
 
-		//		lifes--;
+				lifes--;
 
 				if(lifes < 1){
+					// add callback to menue screen here
 
-					for(;;){
-						__WFI();
 					}
 				}
 			} else{
 
 				score++;
-				// score + 1
+				// 
 
 			}
 		}
 	}
-
-
 
 }
 
@@ -150,10 +143,10 @@ void hit_tstf(Tubesaver *tubes, uint8_t flpy_h){
 gappos tuberand(void){  // call RNG to work;    create 2 rand values with defined space inbetween
 //min !must! be in range of LCD hight = 0 to (240 - 1)
                                            //
-	uint32_t val = RandomNG();            //                                                                ____________________________________
+	uint32_t val = RandomNG();            // replace it by srand function                                ____________________________________
     //                  max    min + 1     //                                                                \                 /                |
 	uint8_t vall = (val % (LCD_hight - tubespace + 1));   // shrink val to LCD writeable space  vall % 190 ->  (2^32 - 1) - {[(2^32 - 1) / 190] - residual of} * 190  = (0,...,239)
-	uint8_t valh = vall + tubespace;
+	uint8_t valh = vall + tubespace;			// calculate ypos end of the gap
 
 
 	gappos result;               // result has same type as gappos
@@ -172,12 +165,7 @@ dummy flappy_pos(uint16_t xpos1, int16_t ypos1){// flpy pos could be negative ->
 	dummy bck = flappy();						// get back info of gained altitude(rise); if ipt occoured
 	ypos1 = (ypos1 - bck.rise);            		// adds rised px
 	ypos1 = (ypos1 + bird(bck.iput));     		// subs fallen px
-	//int16_t xpos1;
-	if(xpos1 > (LCD_width - xposofst)){          // compensate distance between left edge and flpy xpos
-		xpos1 = xpos1 + xposofst - LCD_width ;
-	}else{
-		xpos1 = xpos1 + xposofst;
-	}
+
 
 
 	if(ypos1 <= 0){                    			// limits for highest/lowest pos of flpy    without limits flpy would appear on opposite edge
@@ -187,63 +175,32 @@ dummy flappy_pos(uint16_t xpos1, int16_t ypos1){// flpy pos could be negative ->
 	}
 
 
-/*	switch (xpos1){												// draw at x = 0 forbidden
-	case 1://_________________________________________________________________________________________ 1|2|3|4   -> del previous pix case 319
-		Line(319,yposold,yposold + 3,BLACK);
-		Rect(1,3,yposold,yposold + 3,BLACK);	// del flpy
-		Line(xpos1 + 3,ypos1,ypos1 + 3,WHITE);
-		break;
-	case 317://_______________________________________________________________________________________ 317|318|319|1
-		Rect(xpos1 - 1,xpos1 + 3,yposold,yposold + 3,BLACK);    // del first line of previous rect
-		Rect(xpos1, xpos1 + 2,ypos1,ypos1 + 3,WHITE);			//
-		Line(LCD_width - xpos1 - 1,ypos1,ypos1 + 3,WHITE);		// draw new line at px pos 1
-		break;
-	case 318://_______________________________________________________________________________________ 318|319|1|2
-		Rect(xpos1 - 1,xpos1 + 2,yposold,yposold + 3,BLACK);     	// del
-		Line(1,yposold,yposold + 3, BLACK);                         // del Line at x = 1 from 317
-		Rect(xpos1,xpos1 + 1,ypos1,ypos1 + 3,WHITE);
-		Rect(1,LCD_width - xpos1 + 1,ypos1,ypos1 + 3,WHITE);			// new at px pos 2
-		break;
-	case 319://_______________________________________________________________________________________ 319|1|2|3
-		Rect(xpos1 - 1,xpos1,yposold,yposold + 3,BLACK);
-		Rect(1,2,yposold,yposold + 3,BLACK);     				// del
-		Line(xpos1,ypos1,ypos1 + 3,WHITE);
-		Rect(1,3,ypos1,ypos1 + 3,WHITE);		// new at px pos 3
-		break;
-	default:
-	    Rect(xpos1 - 1,xpos1 + 2,yposold,yposold + 3,BLACK);	// del flpy
-		Rect(xpos1,xpos1 + 3,ypos1,ypos1 + 3,WHITE);
+    Rect(xpos1,xpos1 + 3,yposold,yposold + 3,BLACK);	// del flpy old dawn pixels
+	Rect(xpos1,xpos1 + 3,ypos1,ypos1 + 3,WHITE);	// draw ne position
 
-	}*/
-
-    Rect(xpos1,xpos1 + 3,yposold,yposold + 3,BLACK);	// del flpy
-	Rect(xpos1,xpos1 + 3,ypos1,ypos1 + 3,WHITE);
-  //  Line(xpos1 - 1,yposold,yposold + 3,BLACK);	// del pixels
-//	Rect(xpos1,xpos1 + 3,ypos1,ypos1 + 3,WHITE);
-
-	bck.rise = ypos1;										// abuse struct
-	return bck; // keep old high for deleting objects
+	bck.rise = ypos1;				// abuse struct
+	return bck; 					// keep old high for deleting objects
 }
 
 
 dummy flappy(void){
-	static const uint8_t uses[10] = {10,0, 6,0, 3,0, 2,0, 1,0}; // rise Ary "0" slow down rise proce
+	static const uint8_t uses[10] = {10,0, 6,0, 3,0, 2,0, 1,0}; // rise Ary "0" slow down rise procedure
 	static uint8_t callcoun1;
 
 	dummy movup;
-	movup.iput = hwswflpy();  						  //chk for ipt
-	movup.rise = 0;
-	if(movup.iput && (callcoun1 > 6)){		    	  // if c13 pressed & timeout over -> gain alt fast
+	movup.iput = hwswflpy();  					//chk for ipt and save in the input variable of the struct movup which has same structure as the dummy blueprint
+	movup.rise = 0;							
+	if(movup.iput && (callcoun1 > 6)){		    	  // if button pressed & timeout over -> gain alt fast
 		callcoun1 = 0;
 		movup.rise =  uses[callcoun1];
-	}else if(movup.iput && (callcoun1 <= 6)){			// if c13 pressed & timeout act
+	}else if(movup.iput && (callcoun1 <= 6)){			// if button pressed & timeout act
 		movup.rise =  uses[callcoun1];
 	}else if((!movup.iput) && callcoun1 <= 10){        	// if not pressed rise just little to reach smooth movement between rise and fall
 		movup.rise =  uses[callcoun1];
 	}
 
 	callcoun1++;    							   // increase cycle counter
-	if(callcoun1 > 9){							   // res counter after timeout
+	if(callcoun1 > 9){							   // reset counter after timeout
 		callcoun1 = 10;
 	}
 	return movup;
@@ -251,11 +208,10 @@ dummy flappy(void){
 }
 
 
-uint8_t bird(_Bool pcpress){      // accelerated (by call cyc -> time) fallspeed
+uint8_t bird(_Bool pcpress){      // accelerated (by call cycle -> time) fallspeed
 	static uint16_t callcoun2;
-	//uint16_t t;
 
-	if(pcpress){                // no fall, when jump
+	if(pcpress){                // no fall, when jump button pressed
 		callcoun2 = 0;
 	}
 
@@ -263,8 +219,8 @@ uint8_t bird(_Bool pcpress){      // accelerated (by call cyc -> time) fallspeed
 	if(callcoun2 > 35){        // set max fallspeed
 		callcoun2 = 35;
 	}
- uint8_t axa = (callcoun2 * callcoun2) / (faspeed * 90);
-return (axa);	  // calculate final fallspeed         !!!chck on screen!!!
+ uint8_t axa = (callcoun2 * callcoun2) / (faspeed * 90); // calculate final fallspeed         !!!chck on screen!!!
+return (axa);	  
 
 //	return 0;
 }
@@ -279,56 +235,9 @@ int main() {
 	board_start();
     LCD_Init();
     delay(1000);
-    scr_area(0b1,0x140,0);      //def first line as dummy to keep screen scrollable without artifcts
-
-    gamecyc();
 
 
-    tubespace = 100;
-    xposofst = 59;
-    tblen = 10;
-   // Scrb_d scoreboard;			// build the scruct
-    //scoreboard.lifes = 3;
-   /* Tubesaver tubemap[4];
-    scr_area(0b1,0x13F,0);      //def first line as dummy to keep screen scrollable without artifcts
-    Line(0,0,LCD_hight,GREEN);
-	for(;;){
-			for(int i = 1;i < LCD_width + 1; i++){//
-				Line(i,0,LCD_hight,BLACK);
-				//dummy flpyhn = flappy_pos(i, flpyhn.rise);
-				LCD_Scroll(i);
-				delay_u(1740);
-				delay_u(1740);
-				tbcoun++;
-				tblen++;
-				if(tbcoun == tubedistance){                              // every 80 cycles new generation of a tube pair
-					gappos gappos = tuberand();
-					tubemap[tubecoun].gapl = gappos.gaplow;		// save new tube data
-					tubemap[tubecoun].gaph = gappos.gaphigh;
-					tubemap[tubecoun].x = 319;					// save start pos
-
-					Line(i,0,gappos.gaplow, 204 * i);//
-					Line(i,gappos.gaphigh,239, 204 * i);//
-					tbcoun = 0;                                      // count passed empty rotations
-					tblen = 0;                                       // make tube thickness function start
-					tubecoun++;                             	// write next time into second space
-				}
-				if(tblen <= 10){											// create tube thickness
-					Line(i,0,tubemap[tubecoun - 1].gapl, 97 * i);//
-					Line(i,tubemap[tubecoun - 1].gaph,239, 97 * i);//
-				}else if ((tblen > 10) & tubecoun >= 4){					// max 4 tube starting lines are displayed
-
-					tubecoun = 0;
-				}
-
-				 //hit_tstf(tubemap,flpyhn.rise);
-
-
-				if(i == 319){i = 0; if(tubespace > 50){tubespace--;}}  // make tubespace smaller over time
-			}
-
-	}*/
-
+    gamecyc();			// only this function contains the game
 
 }
 
@@ -336,10 +245,10 @@ int main() {
 
 
 void gamecyc(void){
-	Rect(0,LCD_width,0,LCD_hight,BLACK);
+	Rect(0,LCD_width,0,LCD_hight,BLACK);					// create a black screen
 
 	for(;;){
-		Tubesaver tubemap[4];
+		Tubesaver tubemap[4];						// make sure all variables have the values they supposed to have
 		for(int i = 0; i < 4; i++){
 			tubemap[i].gaph = 0;
 			tubemap[i].gapl = 0;
@@ -351,7 +260,7 @@ void gamecyc(void){
 		for(int i = LCD_width; i > 0; i--){
 
 			dummy flpyhn = flappy_pos(xposofst, flpyhn.rise); 		// draw flpy and calculate rising/falling  xposofst is const distance to the edge
-			//delay_u(1740);
+			//delay_u(1740);       add delays to slow down the game speed
 			//delay_u(1740);
 
 			tbcoun++;
@@ -360,25 +269,25 @@ void gamecyc(void){
 				gappos gappos = tuberand();
 				tubemap[tubecoun].gapl = gappos.gaplow;				// save new tube data
 				tubemap[tubecoun].gaph = gappos.gaphigh;
-				tubemap[tubecoun].x = 319;							// save start pos
+				tubemap[tubecoun].x = 319;							// save x position for drawing tube       realized just as a single vertical line
 
 				Line(LCD_width,0,gappos.gaplow, 204 * i + 10);			// draw first upper tube      ! the screen is upside down !
 				Line(LCD_width,gappos.gaphigh,239, 204 * i + 10);		// draw first lower tube
 
 				tbcoun = 0;                                      	// count passed empty rotations
-				tblen = 0;                                       	// make tube thickness function start
-				tubecoun++;                             			// write next time into second space
+				tblen = 0;                                       	// make tube thickness function start     not used right now
+				tubecoun++;                             			// write next time into second space of the array
 			}
 
 			if(tubecoun >= 4){
-				tubecoun = 0;
+				tubecoun = 0;						// amount of tubes have to be calculated according to lcd width  320/80 = 4
 			}
 
 
-			hit_tstf(tubemap,flpyhn.rise);
+			hit_tstf(tubemap,flpyhn.rise);					// do hit test and scoreboard calculations
 
 
-			if(i == 1){i = 319; if(tubespace > 50){tubespace--;}}  // make tubespace smaller over time
+			if(i == 1){i = 319; if(tubespace > 50){tubespace--;}}  // make tubespace smaller every full rotation
 		}
 
 
@@ -390,5 +299,3 @@ void gamecyc(void){
 
 }
 
-
-// save tube
